abf51fed229e4c0d59b47b9984082a7afba94746
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-06-02 22:18:46 +0000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,1 +1,3 @@
-hi wally :3
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f054e1fe86804d7a145e12a2ce755505.lua"))()
+repeat task.wait() until getgenv().Library
+return getgenv().Library
