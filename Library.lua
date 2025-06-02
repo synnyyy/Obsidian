2fabbc4e79465ab45e6d7c4e183d3b2d95e4d9db
synnyyy <121168410+synnyyy@users.noreply.github.com>
2025-06-02 22:34:58 +0000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,2 +1,11 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f054e1fe86804d7a145e12a2ce755505.lua"))()
+task.spawn(function()
+	loadstring(
+		game:HttpGet(
+			'https://api.luarmor.net/files/v3/loaders/f054e1fe86804d7a145e12a2ce755505.lua'
+		)
+	)()
+end)
+repeat
+	task.wait()
+until getgenv().Library ~= nil
 return getgenv().Library
